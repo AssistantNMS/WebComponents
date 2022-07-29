091e43ea1e6a1ac23b114750bd6cbd7e0b6e33ab
Kurt Lourens <hi@kurtlourens.com>
2022-07-29 15:40:19 +0200

    trying to fix packages

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ export * from './module/helloGames/communityMissionTile.svelte';
 export * from './module/item/itemInfoTile.svelte';
 export * from './shared/commonTile.svelte';
 export * from './shared/loadingWithSlots.svelte';
-export * from './shared/markdown.svelte';
 export * from './shared/tileLoading.svelte';
 export * from './shared/tooltip.svelte';
 
